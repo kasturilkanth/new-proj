ab505517b9c9e868890808cd43dcb711df2aa434
kasturi l <79915454+kasturilkanth@users.noreply.github.com>
2022-07-20 15:20:39 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+$(document).ready(function () {
+  $("#example").DataTable({
+    ajax: "data/objects.txt",
+    columns: [
+      { data: "name" },
+      { data: "position" },
+      { data: "office" },
+      { data: "extn" },
+      { data: "start_date" },
+      { data: "salary" }
+    ]
+  });
+});
